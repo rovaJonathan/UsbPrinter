b8d59f79473dfaded0ec53e5b3e9b7371785d500
jonathan <jonathan@transporttek.org>
2018-08-30 09:57:16 +0300

    add fuction scan device and print

--- src/android/UsbPrinter.java
+++ src/android/UsbPrinter.java
@@ -79,6 +79,169 @@ public class UsbPrinter extends CordovaPlugin {
         }
     }
 
+    final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (device != null) {
+                            //call method to set up device communication
+                            mInterface = device.getInterface(0);
+                            mEndPoint = mInterface.getEndpoint(0);
+                            mConnection = mUsbManager.openDevice(device);
+
+                            //setup();
+                        }
+                    } else {
+                        //Log.d("SUB", "permission denied for device " + device);
+                        Toast.makeText(context, "PERMISSION DENIED FOR THIS DEVICE", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    };
+
+    private void scanDevices(JSONArray args, CallbackContext callback){
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mDeviceList = mUsbManager.getDeviceList();
+
+        if (mDeviceList.size() > 0) {
+            mDeviceIterator = mDeviceList.values().iterator();
+
+            Toast.makeText(this, "Device List Size: " + String.valueOf(mDeviceList.size()), Toast.LENGTH_SHORT).show();
+
+            String usbDevice = "";
+            while (mDeviceIterator.hasNext()) {
+                UsbDevice usbDevice1 = mDeviceIterator.next();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    usbDevice += "\n" +
+                            "DeviceID: " + usbDevice1.getDeviceId() + "\n" +
+                            "DeviceName: " + usbDevice1.getDeviceName() + "\n" +
+                            "Protocol: " + usbDevice1.getDeviceProtocol() + "\n" +
+                            "Product Name: " + usbDevice1.getProductName() + "\n" +
+                            "Manufacturer Name: " + usbDevice1.getManufacturerName() + "\n" +
+                            "DeviceClass: " + usbDevice1.getDeviceClass() + " - " + translateDeviceClass(usbDevice1.getDeviceClass()) + "\n" +
+                            "DeviceSubClass: " + usbDevice1.getDeviceSubclass() + "\n" +
+                            "VendorID: " + usbDevice1.getVendorId() + "\n" +
+                            "ProductID: " + usbDevice1.getProductId() + "\n";
+                }
+
+                int interfaceCount = usbDevice1.getInterfaceCount();
+                Toast.makeText(this, "INTERFACE COUNT: " + String.valueOf(interfaceCount), Toast.LENGTH_SHORT).show();
+
+                mDevice = usbDevice1;
+
+                Toast.makeText(this, "Device is attached", Toast.LENGTH_SHORT).show();
+                callback.success(usbDevice);
+            }
+
+            mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+            IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+            registerReceiver(mUsbReceiver, filter);
+
+            mUsbManager.requestPermission(mDevice, mPermissionIntent);
+        } else {
+            Toast.makeText(this, "Please attach printer via USB", Toast.LENGTH_SHORT).show();
+            callback.error("Please attach printer via USB");
+        }
+
+    }
+
+    private void print(JSONArray args, CallbackContext callback) {
+
+        final String test = args.getJSONObject(0).getString("msg") + "\n\n\n\n\n\n";
+        testBytes = test.getBytes();
+
+        if (mInterface == null) {
+            Toast.makeText(this, "INTERFACE IS NULL", Toast.LENGTH_SHORT).show();
+            callback.error("INTERFACE IS NULL");            
+        } else if (mConnection == null) {
+            Toast.makeText(this, "CONNECTION IS NULL", Toast.LENGTH_SHORT).show();
+            callback.error("CONNECTION IS NULL");    
+        } else if (forceCLaim == null) {
+            Toast.makeText(this, "FORCE CLAIM IS NULL", Toast.LENGTH_SHORT).show();
+            callback.error("FORCE CLAIM IS NULL");
+        } else {
+
+            mConnection.claimInterface(mInterface, forceCLaim);
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    byte[] cut_paper = {0x1D, 0x56, 0x01};
+                    mConnection.bulkTransfer(mEndPoint, testBytes, testBytes.length, 0);
+                    mConnection.bulkTransfer(mEndPoint, cut_paper, cut_paper.length, 0);
+                }
+            });
+            thread.run();
+            callback.success("Impression réussie");
+        }
+    }
+
+    private String translateDeviceClass(int deviceClass) {
+
+        switch (deviceClass) {
+
+            case UsbConstants.USB_CLASS_APP_SPEC:
+                return "Application specific USB class";
+
+            case UsbConstants.USB_CLASS_AUDIO:
+                return "USB class for audio devices";
+
+            case UsbConstants.USB_CLASS_CDC_DATA:
+                return "USB class for CDC devices (communications device class)";
+
+            case UsbConstants.USB_CLASS_COMM:
+                return "USB class for communication devices";
+
+            case UsbConstants.USB_CLASS_CONTENT_SEC:
+                return "USB class for content security devices";
+
+            case UsbConstants.USB_CLASS_CSCID:
+                return "USB class for content smart card devices";
+
+            case UsbConstants.USB_CLASS_HID:
+                return "USB class for human interface devices (for example, mice and keyboards)";
+
+            case UsbConstants.USB_CLASS_HUB:
+                return "USB class for USB hubs";
+
+            case UsbConstants.USB_CLASS_MASS_STORAGE:
+                return "USB class for mass storage devices";
+
+            case UsbConstants.USB_CLASS_MISC:
+                return "USB class for wireless miscellaneous devices";
+
+            case UsbConstants.USB_CLASS_PER_INTERFACE:
+                return "USB class indicating that the class is determined on a per-interface basis";
+
+            case UsbConstants.USB_CLASS_PHYSICA:
+                return "USB class for physical devices";
+
+            case UsbConstants.USB_CLASS_PRINTER:
+                return "USB class for printers";
+
+            case UsbConstants.USB_CLASS_STILL_IMAGE:
+                return "USB class for still image devices (digital cameras)";
+
+            case UsbConstants.USB_CLASS_VENDOR_SPEC:
+                return "Vendor specific USB class";
+
+            case UsbConstants.USB_CLASS_VIDEO:
+                return "USB class for video devices";
+
+            case UsbConstants.USB_CLASS_WIRELESS_CONTROLLER:
+                return "USB class for wireless controller devices";
+
+            default:
+                return "Unknown USB class!";
+        }
+    }
     
 
 }
--- www/UsbPrinter.js
+++ www/UsbPrinter.js
@@ -7,3 +7,11 @@ module.exports.add = function(arg0, success, error){
 module.exports.substract = function(arg0, success, error){
     exec(success, error, 'UsbPrinter', 'substract',[arg0]);
 }
+
+module.exports.scanDevices = function(arg0, success, error){
+    exec(success, error, 'UsbPrinter', 'scanDevices', [arg0]);
+}
+
+module.exports.print = function(arg0, success, error){
+    exec(success, error, 'UsbPrinter', 'print', [arg0]);
+}
