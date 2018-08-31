1eae18e1cdc4d52c9f6fba0eb73c919ae25469eb
jonathan <jonathan@transporttek.org>
2018-08-31 09:44:18 +0300

    remove import

--- src/android/UsbPrinter.java
+++ src/android/UsbPrinter.java
@@ -1,7 +1,5 @@
 package cordova.plugin.usbprinter;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.*;
 
 import org.json.JSONArray;
