1e4e3e383a36a5d2c63979f75165f7ce8cd066f8
jonathan <jonathan@transporttek.org>
2018-08-31 15:20:41 +0300

    clean code

--- src/android/UsbPrinter.java
+++ src/android/UsbPrinter.java
@@ -50,45 +50,10 @@ public class UsbPrinter extends CordovaPlugin {
         }else if(action.equals("print")){
             this.print(args, callbackContext);
             return true;
-        }else if(action.equals("add")){
-            this.add(args, callbackContext);
-            return true;
-        }
-        else if(action.equals("substract")){
-            this.substract(args, callbackContext);
-            return true;
         }
         return false;
     }
 
-
-    private void add(JSONArray args, CallbackContext callback){
-        if(args != null){
-            try{
-                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success(""+(p1+p2));
-            }catch(Exception ex){
-                callback.error("Something went wrong " + ex);
-            }
-        }else{
-            callback.error("Please don't pass null value");
-        }
-    }
-
-    private void substract(JSONArray args, CallbackContext callback){
-        if(args != null){
-            try{
-                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success(""+(p1-p2));
-            }catch(Exception ex){
-                callback.error("Something went wrong " + ex);
-            }
-        }else{
-            callback.error("Please don't pass null value");
-        }
-    }
 
     final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
--- www/UsbPrinter.js
+++ www/UsbPrinter.js
@@ -1,13 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.add = function(arg0, success, error){
-    exec(success, error, 'UsbPrinter', 'add',[arg0]);
-}
-
-module.exports.substract = function(arg0, success, error){
-    exec(success, error, 'UsbPrinter', 'substract',[arg0]);
-}
-
 module.exports.scanDevices = function(arg0, success, error){
     exec(success, error, 'UsbPrinter', 'scanDevices', [arg0]);
 }
